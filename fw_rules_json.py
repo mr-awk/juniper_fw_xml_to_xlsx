be8da3f69ba05dcc310f069c9c0324c2679ea8e5
Marcus Fredlund <marcus.karlson@gmail.com>
2021-04-27 02:40:28 +0200

    Deleted old test file

--- fw_rules_json.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-with open("srx.json") as json_file:
-    data = json.load(json_file)
-    for p in data['address-book']:
-        for a in data['global']:
-            print (a['address'])
