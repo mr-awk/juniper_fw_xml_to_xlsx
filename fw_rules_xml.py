8553313760b66a4c4455b72567a2f6055c7ffcaa
Marcus Fredlund <marcus.karlson@gmail.com>
2021-12-23 12:02:32 +0100

    Fixed alot of trailing whitespaces and other warns

--- fw_rules_xml.py
+++ fw_rules_xml.py
@@ -13,7 +13,7 @@ def main():
     applications = []
     application_set = []
     policies = []
-    
+
     application_in_set = ''
     sub_match_source_addr = ''
     sub_match_dest_addr = ''
@@ -25,7 +25,7 @@ def main():
     protocol = ''
 
     writer = pd.ExcelWriter('test.xlsx', engine='xlsxwriter')
-    
+
     for root_item in root:
         if root_item.tag == 'configuration':
             for conf_item in root_item:
@@ -47,9 +47,10 @@ def main():
 
                                     # When collected all addresses, save it to excel
                                     addresses.append([name] + [description] + [ip_prefix])
-                                    df1 = pd.DataFrame(addresses, columns=['Name','Description','IP Address'])
+                                    df1 = pd.DataFrame( addresses,
+                                                        columns=['Name','Description','IP Address'])
                                     df1.to_excel(writer, index=False, sheet_name='Addresses')
-                                
+
                                 if address_book_item.tag == 'address-set':
                                     for address_set in address_book_item:
                                         if address_set.tag == 'name':
@@ -59,14 +60,17 @@ def main():
                                             description = address_set.text
 
                                         if address_set.tag == 'address':
-                                            addresses_in_set = addresses_in_set + address_set[0].text + '\n'
-                                    
+                                            addresses_in_set =  (addresses_in_set +
+                                                                address_set[0].text +
+                                                                '\n')
+
                                     # When collected all address-sets, save it to excel
                                     address_sets.append([name] + [description] + [addresses_in_set])
                                     addresses_in_set = ''
-                                    df2 = pd.DataFrame(address_sets, columns=['Name','Description','Addresses'])
+                                    df2 = pd.DataFrame( address_sets,
+                                                        columns=['Name','Description','Addresses'])
                                     df2.to_excel(writer, index=False, sheet_name='Address-sets')
-                        
+
                         # TODO: Fix this part method
                         if sec_item.tag == 'policies':
                             print("Reading policies in policies")
@@ -77,7 +81,7 @@ def main():
                                             from_zone_name = policy_item.text
                                         if policy_item.tag == 'to-zone-name':
                                             to_zone_name = policy_item.text
-                                        
+
                                         if policy_item.tag == 'policy':
                                             for subpolicy in policy_item:
                                                 if subpolicy.tag == 'name':
@@ -85,18 +89,34 @@ def main():
                                                 if subpolicy.tag == 'match':
                                                     for match in subpolicy:
                                                         if match.tag == 'source-address':
-                                                            sub_match_source_addr = sub_match_source_addr + match.text + '\n'
+                                                            sub_match_source_addr = (sub_match_source_addr +
+                                                                                     match.text +
+                                                                                     '\n')
                                                         if match.tag == 'destination-address':
-                                                            sub_match_dest_addr = sub_match_dest_addr + match.text + '\n'
+                                                            sub_match_dest_addr = ( sub_match_dest_addr +
+                                                                                    match.text +
+                                                                                    '\n')
                                                         if match.tag == 'application':
                                                             sub_match_app = sub_match_app + match.text + '\n'
-                                            policies.append([from_zone_name] + [to_zone_name] + [subpol_name] + [sub_match_source_addr] + [sub_match_dest_addr] + [sub_match_app])
+                                            policies.append(    [from_zone_name] +
+                                                                [to_zone_name] +
+                                                                [subpol_name] +
+                                                                [sub_match_source_addr] +
+                                                                [sub_match_dest_addr] +
+                                                                [sub_match_app])
                                             sub_match_app = ''
                                             sub_match_dest_addr = ''
                                             sub_match_source_addr = ''
 
-                                    # When collected all policies, save it to excel                                  
-                                    df3 = pd.DataFrame(policies, columns=['from-zone-name','to-zone-name','name','source-address','destination-address','application'])
+                                    # When collected all policies, save it to excel
+                                    df3 = pd.DataFrame( policies,
+                                                        columns=[   'from-zone-name',
+                                                                    'to-zone-name',
+                                                                    'name',
+                                                                    'source-address',
+                                                                    'destination-address',
+                                                                    'application']
+                                                                    )
                                     df3.to_excel(writer, index=False, sheet_name='Policies')
 
                 if conf_item.tag == 'applications':
@@ -118,14 +138,22 @@ def main():
                                             term_label_dest_port = term.text
                                         if term.tag == 'protocol':
                                             term_label_protocol = term.text
-                                        
+
                                         if len(term_label_dest_port) > 0 and len(term_label_protocol) > 0:
-                                            term_label = term_label + term_label_dest_port + '/' + term_label_protocol + '\n'
+                                            term_label = (  term_label +
+                                                            term_label_dest_port +
+                                                            '/' +
+                                                            term_label_protocol +
+                                                            '\n')
                                             term_label_dest_port = ''
                                             term_label_protocol = ''
 
                             # When collected all applications, save it to excel
-                            applications.append([name] + [source_port] + [dest_port] + [protocol] + [term_label])
+                            applications.append(    [name] +
+                                                    [source_port] +
+                                                    [dest_port] +
+                                                    [protocol] +
+                                                    [term_label])
                             term_label = ''
                             protocol = ''
                             dest_port = ''
@@ -133,7 +161,13 @@ def main():
                             term_label_dest_port = ''
                             term_label_protocol = ''
 
-                            df4 = pd.DataFrame(applications, columns=['Name','Source port','Destination port','Protocol','Destination ports/protocol'])
+                            df4 = pd.DataFrame( applications,
+                                                columns=[   'Name',
+                                                            'Source port',
+                                                            'Destination port',
+                                                            'Protocol',
+                                                            'Destination ports/protocol']
+                                                            )
                             df4.to_excel(writer, index=False, sheet_name='Applications')
 
                         if application.tag == 'application-set':
@@ -144,10 +178,9 @@ def main():
                                     application_in_set = application_in_set + app_set_item[0].text + '\n'
                             application_set.append([name] + [application_in_set])
                             application_in_set = ''
-                            
+
                             df5 = pd.DataFrame(application_set, columns=['Name','Applications'])
                             df5.to_excel(writer, index=False, sheet_name='Application-sets')
     writer.save()
 if __name__ == '__main__':
     main()
-
